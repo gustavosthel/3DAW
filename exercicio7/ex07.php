30a6bd060d49adf4e19d5f7b77a28bb8fb7a96df
Gustavo Sthel <gustavosthel@gmail.com>
2024-10-01 16:18:29 -0300

    corrigindo erros e finalizando exercicio

--- exercicio7/ex07.php
+++ exercicio7/ex07.php
@@ -2,20 +2,21 @@
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $matricula = $_GET["matricula"];
     $nome = $_GET["nome"];
-    $email = $_GET["senha"];
+    $senha = $_GET["senha"];
 
-//  Vou escrever os dados do formulário em um arquivo de dados já existente
 if (!file_exists("alunos.txt")) {
-    $arqDisc = fopen("alunos.txt","w") or die("erro ao criar arquivo");
+    $arqDisc = fopen("alunos.txt", "w") or die("Erro ao criar arquivo");
     $linha = "matricula;nome;senha\n";
-    fwrite($arqDisc,$linha);
+    fwrite($arqDisc, $linha);
     fclose($arqDisc);
 }
-$arqDisc = fopen("alunos.txt","a") or die("erro ao abrir arquivo");
 
- $linha = $matricula . ";" . $nome . ";" . $senha . "\n";
- fwrite($arqDisc,$linha);
- fclose($arqDisc);    echo "Aluno inserido com sucesso!";
+$arqDisc = fopen("alunos.txt", "a") or die("Erro ao abrir arquivo");
+
+$linha = $matricula . ";" . $nome . ";" . $senha . "\n";
+fwrite($arqDisc, $linha);
+fclose($arqDisc);
+
+echo "Aluno inserido com sucesso!";
 }
-
 ?>
--- exercicio7/javascript.js
+++ exercicio7/javascript.js
@@ -1,19 +1,22 @@
-function adicionarAluno() {
-    const matricula = document.getElementById("matricula").value
-    const nome = document.getElementById("nome").value
-    const senha = document.getElementById("senha").value
-    const xmlhttp = new XMLHttpRequest()
+document.getElementById("formsAluno").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const matricula = document.getElementById("matricula").value;
+    const nome = document.getElementById("nome").value;
+    const senha = document.getElementById("senha").value;
+
+    const xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             console.log("Chegou a resposta OK: " + this.responseText);
             document.getElementById("formsAluno").innerHTML = this.responseText;
-        } else {
-            console.log("Requisicao falhou: " + this.status);
+        } else if (this.readyState == 4) {
+            console.log("Requisição falhou: " + this.status);
         }
     }
 
-    xmlhttp.open("GET", "http://localhost/3DAW/exercicio7/ex07.php=" + matricula +
-                            "&nome=" + nome + "&email=" + senha);
-    xmlhttp.send()
-}
+    xmlhttp.open("GET", "http://localhost/3DAW/exercicio7/ex07.php?matricula=" + matricula +
+                            "&nome=" + nome + "&senha=" + senha, true);
+    xmlhttp.send();
+});
